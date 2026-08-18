b0315a5aeebdabbdff79663c79957f13cbb3dd10
yarobkl <eliebakala@gmail.com>
2026-08-18 17:38:56 +0200

    Restore mobile navigation before swipe prototype

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import CartRoute from './CartRoute'
 import { Cgv, Confidentialite, Cookies, MentionsLegales } from './LegalPages'
 import SiteLegalStrip from './SiteLegalStrip'
 import SeoManager from './SeoManager'
-import SwipeNavigator from './SwipeNavigator'
 import './styles.css'
 import './stability.css'
 import './payment-flow.css'
@@ -19,7 +18,6 @@ import './home-live-commerce'
 import './site-contact-fix'
 import './cart-terminology'
 import './fluidity.css'
-import './swipe-navigation.css'
 
 type ErrorBoundaryState = { hasError: boolean }
 
@@ -82,7 +80,6 @@ ReactDOM.createRoot(root).render(
       ) : (
         <BrowserRouter>
           <SeoManager />
-          <SwipeNavigator />
           <Routes>
             <Route path="/produits" element={<CatalogRoute />} />
             <Route path="/click-collect" element={<CartRoute />} />
